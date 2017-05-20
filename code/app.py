5ac0633057ca662476ca878abb420884b01b62a5
Danis Fermi <danisfermijohn@gmail.com>
2017-05-20 12:41:24 -0400

    Added Base Get Request and Memory DB

--- code/app.py
+++ code/app.py
@@ -3,9 +3,30 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Index
 @app.route('/')
 def index():
 	return "Hello World"
+
+# Base GET Request
+@app.route('/contacts', method=['GET'])
+def get_contacts():
+	return jsonify({'contacts':contacts})
+
+# Memory Database
+# JSON Formatted. Fields are:-
+#					+Name (string)
+# 				+Mail (string)
+contacts = [
+							{
+								'name': 'Danis',
+								'mail': 'dfermi@ncsu.edu'
+							},
+							{
+								'name': 'Rohit',
+								'mail': 'rnambis@ncsu.edu'
+							}
+						]
 
 if __name__ == '__main__':
 	app.run(debug=True)
