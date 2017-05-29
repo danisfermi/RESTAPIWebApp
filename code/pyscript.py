757d6364b2ccbeb5f8b7443d118b6bde048454a3
Danis Fermi <danisfermijohn@gmail.com>
2017-05-29 16:43:28 +0530

    Basic UI for Python Script Task 2

--- code/pyscript.py
+++ code/pyscript.py
@@ -1,11 +1,15 @@
+#!/usr/bin/python
 import urllib2
 
-print "Python Script to Interact with the REST API"
-print "-------------------------------------------"
-print "The following choices are available:-"
-print "1, GET Request to fetch all data"
-print "2. GET Request to fetch specific data"
-print "3. POST Request to insert a record"
-print "4. PUT Request to update a record"
-print "5. DELETE Request to delete a record"
-raw_input("Enter your choice: ")
+cont = 'Yes'
+while (cont=='yes' or cont=='Yes' or cont=='Y' or cont=='y' or cont=='YES'):
+	print "Python Script to Interact with the REST API"
+	print "-------------------------------------------"
+	print "The following choices are available:-"
+	print "1, GET Request to fetch all data"
+	print "2. GET Request to fetch specific data"
+	print "3. POST Request to insert a record"
+	print "4. PUT Request to update a record"
+	print "5. DELETE Request to delete a record"
+	ch = raw_input("Enter your choice: ")
+
