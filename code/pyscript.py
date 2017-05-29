bc36d2527a241714840bda0a6b3b548d779a7d5b
Danis Fermi <danisfermijohn@gmail.com>
2017-05-29 16:33:54 +0530

    Adding Python Script to Interact with API - Task 2

---
+++ code/pyscript.py
@@ -0,0 +1,11 @@
+import urllib2
+
+print "Python Script to Interact with the REST API"
+print "-------------------------------------------"
+print "The following choices are available:-"
+print "1, GET Request to fetch all data"
+print "2. GET Request to fetch specific data"
+print "3. POST Request to insert a record"
+print "4. PUT Request to update a record"
+print "5. DELETE Request to delete a record"
+raw_input("Enter your choice: ")
